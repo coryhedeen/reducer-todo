1f3af2194320eaa6790822615cf0e335ec412309
Cory Hedeen <hedeencory@gmail.com>
2019-08-13 15:48:14 -0400

    Render form

---
+++ todo-list/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListForm from './components/ListForm';
+
+function App() {
+  return (
+    <div className="App">
+      <ListForm />
+    </div>
+  );
+}
+
+export default App;
